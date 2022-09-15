0dbae5971aba9e30249addc5a50f7a6f1bd07dd1
ecko2010 <ahmedegy04@gmail.com>
2022-09-15 20:52:52 +0400

    Update ar.js

--- language/ar.js
+++ language/ar.js
@@ -1,1 +1,18 @@
+var language = {
+	"wallet": "محفظة",
+	"market": "السوق",
+	"send": "ارسال",
+	"get": "Get",
+	"settings": "الاعدادات",
+	"back": "للخلف",
+	"helloworld": "اهلا بكم",
+	"deposit": "Deposit",
+	"withdraw": "Withdraw",
+	"language": "اللغة",
+	"currency": "Сurrency",
+	"requisites": "Requisites",
+	"referrals": "Referrals",
+	"addcard": "Add card",
+}
 
+module.exports = language
